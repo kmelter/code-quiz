a5838eadded67d6be969348f8138429bc6b28450
kmelter <kmelter97@gmail.com>
2022-02-26 18:27:30 -0600

    add createButtons() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,19 @@ var questionText = document.getElementById('question');
 var answerArea = document.getElementById('main');
 var answerResult = document.getElementById('result');
 
+var answerElements = [];
+
 var question1 = {
     question: "Commonly used data types DO NOT include:",
-    answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
-}
+    answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    correctAnswer: "3"
+};
+
+var question2 = {
+    question: "The condition of an if / else statement is enclosed with____.",
+    answers: ["1. quotes", "2. curly brackets", "3. paranthesis", "4. square brackets"],
+    correctAnswer: "3"
+};
 
 
 
@@ -38,6 +47,15 @@ function countdown() {
     }, 1000);
 }
 
+function createButtons() {
+    // makes empty buttons to use for each question
+    for (i = 0; i < question1.answers.length; i++) {
+        var answerButton = document.createElement("button");
+        answerButton.id = i + 1;
+        answerElements.push(answerButton);
+    }
+}
+
 function askQuestion1() {
     
     // CLEAR CONTENTS
@@ -45,30 +63,37 @@ function askQuestion1() {
     
     // display the question
     questionText.textContent = question1.question
-    // make buttons for each of the four answers
+    // display buttons for each of the four answers
     for (i = 0; i < question1.answers.length; i++) {
-        var answerButton = document.createElement("button");
-        answerButton.textContent = question1.answers[i];
-        answerButton.className = i + 1;
-        answerArea.appendChild(answerButton);
+        answerElements[i].textContent = question1.answers[i];
+        answerArea.appendChild(answerElements[i]);
     }
     
-    // if user selects correct answer
-    if (document.getElementsByClassName('3').clicked === true) {
-        answerResult.textContent = "Correct";
-        question2();
-    }
-    // if user selects incorrect answer
-    // * countdown -10 seconds (timeLeft) if wrong
-    if (document.getElementsByClassName('1', '2', '4').clicked === true) {
-        answerResult.textContent = "incorrect";
-        timeLeft = timeLeft - 10;
-        // DECIDE WHETHER OR NOT USER GETS ANOTHER CHANCE TO ANSWER CORRECTLY OR MOVE ON TO NEXT QUESTION
+    
+    //for each element in answerElements
+        //if the element id === the correct answer id
+        //then add event listener for correct answer
+        //else add event listener for incorrect answer
+
+    for (i = 0; i < answerElements.length; i++) {
+        if (answerElements[i].id === question1.correctAnswer) {
+            answerElements[i].addEventListener("click", function() {
+                answerResult.textContent = "Correct";
+                // go to the next question
+            });
+        }
+        else {
+            answerElements[i].addEventListener("click", function() {
+                answerResult.textContent = "incorrect";
+                timeLeft = timeLeft - 10;
+            });
+        }
     }
 }
 
 function gameStart() {
     countdown();
+    createButtons();
     askQuestion1();
 }
 
